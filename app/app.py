04e8e59ad927109c4d0e2c23598fe5734394003c
markson <marksonaigbodi@bpp.com>
2026-07-02 13:11:50 +0100

    add more deployment settings

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from GitHub Actions CI/CD! from github"
+    return "Hello from GitHub Actions CI/CD! from github with token"
 
 @app.route("/health")
 def health():
