a2551772fd0ba3b39bd3d8078c1c71cc9cdf4b53
markson <marksonaigbodi@bpp.com>
2026-07-02 13:07:16 +0100

    add more content

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from GitHub Actions CI/CD!"
+    return "Hello from GitHub Actions CI/CD! from github"
 
 @app.route("/health")
 def health():
